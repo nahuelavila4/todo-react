9ae99260a7f45720e0ff0531a558bfbc6b0b282f
Nahuel Avila <avilanahuel99@gmail.com>
2022-11-12 19:33:00 -0300

    funciones basicas

--- src/componentes/Contenedor-Tareas.js
+++ src/componentes/Contenedor-Tareas.js
@@ -4,24 +4,51 @@ import TareaFormulario from "./Formulario";
 import { Tareas } from "./Tareas";
 
 export function ContenedorTareas() {
+  // Lista de tareas de el estado
   const [tareas, setTareas] = useState([]);
 
-  const agregarTareas = tarea => {
-    console.log("Tarea agregada")
-    console.log(tarea)
-  }
+  // El arg tarea recibe datos de
+  // prop.onSubmit(tareaNueva)
+  const agregarTareas = (tarea) => {
+    // Para verif que no este en blanco
+    // trim saca esp del principio y final de string
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  };
+
+  // filter agrega todos los elementos que cumplen la condicion
+  const eliminarTarea = (id) => {
+    const tareasActualizadas = tareas.filter((tarea) => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  };
 
   return (
     <div className="tarea-contenedor">
-      <TareaFormulario />
-      <div className="tareas-conjunto">
-        {tareas.map((tarea) => (
-          <Tareas texto={tarea.texto} completada={tarea.completada} />
-        ))}
+      {/* Envia funcion a comp donde se hace la tarea */}
+      <div>
+        <TareaFormulario onSubmit={agregarTareas} />
+        <div className="tareas-conjunto">
+          {tareas.map((tarea) => (
+            <Tareas
+              eliminarTarea={eliminarTarea}
+              key={tarea.id}
+              id={tarea.id}
+              texto={tarea.texto}
+              completada={tarea.completada}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
 }
+
+// Para mostrar lista en React y este
+// respete el orden hay que darle prop key,
+// sirve para identificar elemento en la lista
 
 /* 
 Map recorre las tareas y las pasa como argumento a una funcion.
--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "../stylesheet/Formulario.css";
 
 function TareaFormulario(props) {
+  //manejarCambio cambia el value texto de tareaNueva
+  //con lo que el usuario haya escrito hasta el momento
+  const [input, setInput] = useState("");
 
-    //manejarCambio modifica el value texto de tareaNueva
-    //con lo que el usuario haya escrito hasta el momento
-    const manejarCambio = (e) => {};
-  
+  const manejarCambio = (e) => {
+    setInput(e.target.value);
+    // Permite extraer valor del input
+  };
+
   const manejarEnvio = (e) => {
+    e.preventDefault();
+    console.log("Enviando");
+
     const tareaNueva = {
-      id: "32",
-      // texto:
+      id: uuidv4(),
+      texto: input,
+      completada: false,
     };
+    props.onSubmit(tareaNueva)
+    // Envia datos del obj
   };
 
   return (
-    <form className="tarea-form">
+    <form className="tarea-form" onSubmit={manejarEnvio}>
       <input
+        type="text"
         className="tarea-descripcion"
         placeholder="Ingrese su tarea"
         name="texto"
+        onChange={manejarCambio}
       />
       <button className="tarea-boton">Agregar</button>
     </form>
